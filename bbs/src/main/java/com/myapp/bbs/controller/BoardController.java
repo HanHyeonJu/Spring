9d2368576b09594c91fd2fe9e11d8f6e85b1799e
hhj <ha10155@naver.com>
2022-04-18 12:47:02 +0900

    0418_mybatis게시판

--- bbs/src/main/java/com/myapp/bbs/controller/BoardController.java
+++ bbs/src/main/java/com/myapp/bbs/controller/BoardController.java
@@ -5,16 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myapp.bbs.model.BoardVO;
 import com.myapp.bbs.service.BoardService;
 
-import lombok.extern.java.Log;
-
 @Controller
 @RequestMapping("/board")
-@Log
 public class BoardController {
 
 	private BoardService boardService;
@@ -25,8 +23,8 @@ public class BoardController {
 	}
 
 	@GetMapping("/list")
-	public String boardListGet() {
-		log.info("게시판 리스트 페이지 진입");
+	public String boardListGet(Model model) {
+		model.addAttribute("boardList", boardService.getList());
 		return "list";
 	}
 	
@@ -37,10 +35,44 @@ public class BoardController {
 	}
 	
 	@PostMapping("/enroll")
-	public String boardEnrollGet(BoardVO board, RedirectAttributes attr) {
+	public String boardEnrollPost(BoardVO board, RedirectAttributes attr) {
 //		log.info("BoardVO : " + board);
 		boardService.enroll(board);
 		attr.addFlashAttribute("message", "게시글 등록 성공");
 		return"redirect:/board/list";
+	}
+	
+	/**
+	 * 게시판 글을 조회하기
+	 * @param bno
+	 * @param model
+	 * @return
+	 * 
+	 */
+	@GetMapping("/get")
+	public String boardPageGET(@RequestParam("bno") int bno, Model model) {
+		model.addAttribute("board", boardService.getPage(bno));
+		return "get";
+	}
+	
+	@GetMapping("/modify")
+	public String boardModifyGET(@RequestParam("bno") int bno, Model model) {
+		model.addAttribute("board", boardService.getPage(bno));
+		return "modify";
+	}
+	
+	@PostMapping("/modify")
+	public String boardModifyPost(BoardVO board, RedirectAttributes attr) {
+		boardService.modify(board); // modify 이미지에서 수정된 내용을 업데이트함
+		attr.addFlashAttribute("message", "수정성공!");
+		// post - redirect - get
+		return"redirect:/board/list"; // redirect를 해주지 않으면 새로고침 했을 때 중복으로 post될 수 있음 그래서 아예 새로운 요청으로 보내버림
+	}
+	
+	@GetMapping("/delete")
+	public String boardDeleteGet(@RequestParam("bno") int bno,  RedirectAttributes attr) {
+		boardService.delete(bno);
+		attr.addFlashAttribute("message", "삭제성공!");
+		return "redirect:/board/list";
 	}
 }
--- bbs/src/test/java/com/myapp/bbs/dao/BoardMapperTest.java
+++ bbs/src/test/java/com/myapp/bbs/dao/BoardMapperTest.java
@@ -7,25 +7,66 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.annotation.Rollback;
 
-import com.myapp.bbs.model.BoardVO;
+//import com.myapp.bbs.model.BoardVO;
+
+import lombok.extern.java.Log;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = Replace.NONE) // 실제 DB로 테스트
-@Rollback(value = false) // 테스트할 때 롤백 처리 하지 않음ㄴ
+@Rollback(value = false) // 테스트할 때 롤백 처리 하지 않음
+@Log
 public class BoardMapperTest {
 	
 	// JUNIT 5버전 테스트
 	@Autowired
 	private BoardMapper boardMapper;
 	
+	// 테스트 한 것들은 주석처리함
 	@Test
 	public void test() {
-		BoardVO vo = new BoardVO();
+		/* 게시판 등록 */
+//		BoardVO vo = new BoardVO();
+//		
+//		vo.setTitle("제목테스트");
+//		vo.setContent("내용테스트");
+//		vo.setWriter("글쓴이");
+//		
+//		boardMapper.enroll(vo);
 		
-		vo.setTitle("제목테스트");
-		vo.setContent("내용테스트");
-		vo.setWriter("글쓴이");
+		/* 게시판 조회 */
+//		List<BoardVO> list = boardMapper.getList();
+//		// 반복문을 써서 테스트
+//		for(int i=0; i < list.size(); i++) {
+//			log.info("" + list.get(i));
+//		}
 		
-		boardMapper.enroll(vo);
+		/* 향상된 foreach문*/
+		/*for(BoardVO vo : list) {
+			log.info("" + vo);
+		}*/
+		
+		/* foreach 메소드와 람다식 */
+		/*list.forEach(board -> log.info("" + board));*/
+		
+		/* 게시판 조회 */
+//		int bno = 1;
+//		log.info(""+boardMapper.getPage(bno));
+		
+		/* 게시판 수정 */
+//		BoardVO board = new BoardVO();
+//		board.setBno(1);
+//		board.setTitle("수정된 제목");
+//		board.setContent("수정된 게시글 내용");
+//		
+//		int result = boardMapper.modify(board);
+//	
+//		log.info("결과: " + result);
+		
+		/* 게시판 삭제 */
+		int result = boardMapper.delete(5);
+		log.info("결과: " + result);
+		
 	}
+	
+	
 }
